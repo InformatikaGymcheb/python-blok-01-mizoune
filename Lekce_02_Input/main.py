0615c3624846aa4c953e8a9e572b2f5ac1834c62
mizoune <mizoune@gymcheb.cz>
2026-03-19 13:18:09 +0100

    Prosim ze je to spravne

--- Lekce_02_Input/main.py
+++ Lekce_02_Input/main.py
@@ -3,14 +3,32 @@
 # ==========================================
 # Sem napiš svůj kód:
 
+jmeno = input("Zadej své jméno: ")
+barva = input("Jaká je tvoje oblíbená barva? ")
+
+print(f"Ahoj {jmeno}, tvoje oblíbená barva je {barva}.")
+
+print()  
 
 # ==========================================
 # ÚLOHA 2: Jednoduchá kalkulačka (součet)
 # ==========================================
 # Sem napiš svůj kód:
+# 2. Úloha: Jednoduchá kalkulačka
+cislo1 = int(input("Zadej první číslo: "))
+cislo2 = int(input("Zadej druhé číslo: "))
 
+soucet = cislo1 + cislo2
+print(f"Součet je: {soucet}")
+
+print()
 
 # ==========================================
 # ÚLOHA 3: Výpočet psího věku (věk * 7)
 # ==========================================
 # Sem napiš svůj kód:
+# 3. Úloha: Výpočet věku psa
+vek = int(input("Kolik ti je let? "))
+
+psi_vek = vek * 7
+print(f"V psích letech by ti bylo: {psi_vek} let. Haf!")
